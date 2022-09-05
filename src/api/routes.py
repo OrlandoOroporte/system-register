c344726620c2e02ac9e12c08cb82df0d7d79a1ad
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-09-05 12:36:12 +0000

    add route login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,15 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
+def set_password(password):
+    return generate_password_hash(password)
+
+def check_password(hash_password, password, salt):
+    return check_password_hash(hash_password, password) #### devuelve true o false si la contresaña  coincide 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -26,6 +32,8 @@ def add_user():
         if email is None or password is None:
             return jsonify('All fields are required'), 400
         else:
+            password = set_password(password)
+            print(password)
             request_user = User(email=email, password=password, salt="12345") ##se podria hacer con el metodo init
             db.session.add(request_user)
 
@@ -44,13 +52,14 @@ def login():
         email = body.get('email', None)
         password = body.get('password', None)
 
-        login_user = User.query.filter_by(email=email, password=password).one_or_none() ### este metodo es para traer un solo usuario. 
+        login_user = User.query.filter_by(email=email).one_or_none() ### este metodo es para traer un solo usuario. //solo se verifica el email por el password esta hash
 
         if login_user:
-            print('tiene persmiso')
-            ##crear eltoken aqui
-            return jsonify('esta logeado'),200
-        else:
-            return jsonify('bad credential'),400
+            if check_password(login_user.password, password, login_user.salt):
+                print('access granted')
+                ##crear el token aqui
+                return jsonify('access granted'),200
+            else:
+                return jsonify('bad credential'),400
 
     return jsonify('todobien'),201
