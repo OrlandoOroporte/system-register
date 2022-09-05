daa19b0d348faa235791c9e86843cb9c57d5e0e8
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-09-05 16:56:55 +0000

    add navbar and login pages

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,20 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os 
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from werkzeug.security import generate_password_hash, check_password_hash
+from base64 import b64encode
 
 api = Blueprint('api', __name__)
 
-def set_password(password):
-    return generate_password_hash(password)
+def set_password(password,salt):
+    return generate_password_hash(f"{password}{salt}")
 
 def check_password(hash_password, password, salt):
-    return check_password_hash(hash_password, password) #### devuelve true o false si la contresaña  coincide 
+    return check_password_hash(hash_password, f"{password}{salt}") #### devuelve true o false si la contresaña  coincide 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -32,9 +34,9 @@ def add_user():
         if email is None or password is None:
             return jsonify('All fields are required'), 400
         else:
-            password = set_password(password)
-            print(password)
-            request_user = User(email=email, password=password, salt="12345") ##se podria hacer con el metodo init
+            salt = b64encode(os.urandom(32).decode('utf-8'))
+            password = set_password(password, salt)
+            request_user = User(email=email, password=password, salt=salt) ##se podria hacer con el metodo init
             db.session.add(request_user)
 
             try:
@@ -52,11 +54,10 @@ def login():
         email = body.get('email', None)
         password = body.get('password', None)
 
-        login_user = User.query.filter_by(email=email).one_or_none() ### este metodo es para traer un solo usuario. //solo se verifica el email por el password esta hash
+        login_user = User.query.filter_by(email=email).one_or_none() ### este metodo es para traer un solo usuario. //solo se verifica el email por el password esta hashed
 
         if login_user:
             if check_password(login_user.password, password, login_user.salt):
-                print('access granted')
                 ##crear el token aqui
                 return jsonify('access granted'),200
             else:
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,65 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+	  <Link to="/" className="navbar-brand">
+      <img src="../../img/react-img.png" alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+      React Boilerplate
+    </Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="" className="nav-link">
+                Images 
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+			  	to=""
+                className="nav-link dropdown-toggle"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Action User
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link to="/Register" className="dropdown-item">
+                    Regiter User
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Login"  className="dropdown-item">
+                   Login User 
+                  </Link>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <Link to="" className="dropdown-item">
+                    Something else here
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
