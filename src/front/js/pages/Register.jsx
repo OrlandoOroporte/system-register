2437f0210557bf0a491a88ec4ea73c2742ffaf5e
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-09-07 20:14:51 +0000

    add funtion register user in data base

--- src/front/js/pages/Register.jsx
+++ src/front/js/pages/Register.jsx
@@ -2,18 +2,54 @@ import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
+
 const Register = () => {
+
+  let initialState = {
+    email: "",
+    password: "",
+  };
+
+  const { actions } = useContext(Context); /// vamos a crear la funcion de registro desde los acction( esta funcion pudiera estar en page por que solo se vera desde aqui)
+  const [userRegister, setUserRegister] = useState(initialState);
+  
+  const handleChange = ({ target }) => {
+    //para sincronizar los inputs 
+    setUserRegister({
+      ...userRegister,
+      [target.name]: target.value,
+    });
+  };
+
+  ///funcion para recibir los datos del usuraio y resgitarlo en base datos
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); //para evitar el burbujeo 
+    if (userRegister.email.trim() != "" && userRegister.password.trim() != ""){ /// validacion para solictar inf a en los campos email Y password 
+      console.log("registramos");}
+    
+    else {
+      console.log("todos los campos son obligatorios")
+    } 
+
+
+  
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label for="exampleInputEmail1" className="form-label">
           Email address
         </label>
         <input
           type="email"
+          name="email"
           className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
+          onChange={handleChange}
+          value={userRegister.email}
         />
         <div id="emailHelp" className="form-text">
           We'll never share your email with anyone else.
@@ -25,8 +61,11 @@ const Register = () => {
         </label>
         <input
           type="password"
+          name="password"
           className="form-control"
           id="exampleInputPassword1"
+          onChange={handleChange}
+          value={userRegister.password}
         />
       </div>
       <div className="mb-3 form-check">
